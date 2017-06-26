17bb7f7d52e58193781e5268df8499b59d62865e
Steve Licata <Steve.Licata@gmail.com>
2017-06-26 18:54:41 -0400

    generating random styles/fonts/colors w AngularJS

--- js/text.js
+++ js/text.js
@@ -2,4 +2,20 @@ var textapp = angular.module("textapp", []);
 
 textapp.controller('textCtrl', function($scope){
 
+  var fonts = [
+    "cursive", "fantasy", "monospace", "serif", "sans-serif"
+  ];
+
+  var colors = [
+    "red", "black", "orange", "blue", "purple", "white", "gray"
+  ];
+
+  var style = [
+    "italic", "bold"
+  ];
+
+  $scope.randomFont = fonts[Math.floor((Math.random() * 5))];
+  $scope.randomColor = colors[Math.floor((Math.random() * 7))];
+  $scope.randomStyle = style[Math.floor((Math.random() * 2))]; 
+
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-AngularJS application changes the CSS styling of text. 
+Application changes the CSS styling of text and displays CSS code using AngularJS. 
